e349a255b3eb6f4a794a2c93b94dec6622501d01
Fahad Qureshi <qureshifahad984@gmail.com>
2026-07-29 21:50:07 +0500

    Update multiple_disease_prediction.py

--- Deployed_Models/multiple_disease_prediction.py
+++ Deployed_Models/multiple_disease_prediction.py
@@ -4,11 +4,11 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # Loading saved Models
-diabetes_disease_model = pickle.load(open('C:/Users/Fahad Qureshi/Downloads\Deployed_Models/diabetes_model.sav', 'rb'))
+diabetes_disease_model = pickle.load(open('saved_models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/Fahad Qureshi/Downloads/Deployed_Models/heart_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('saved_models/heart_model.sav', 'rb'))
 
-parkinsons_disease_model = pickle.load(open('C:/Users/Fahad Qureshi/Downloads/Deployed_Models/parkinson_model.sav', 'rb'))
+parkinsons_disease_model = pickle.load(open('saved_models/parkinson_model.sav', 'rb'))
 
 # Sidebar for Navigation
 with st.sidebar:
